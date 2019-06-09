1e7c4eefcc65a16f55b5d5efde864b48096cc86f
rbauwens <ruthbauwens@hotmail.com>
2019-06-09 15:27:36 +0100

    Add page for selecting photos from device

---
+++ app/src/main/java/com/example/cheermeup/activity/SelectPhotoActivity.java
@@ -0,0 +1,17 @@
+package com.example.cheermeup.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cheermeup.R;
+
+public class SelectPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_photo);
+
+    }
+}
--- app/src/main/java/com/example/cheermeup/fragment/SettingsFragment.java
+++ app/src/main/java/com/example/cheermeup/fragment/SettingsFragment.java
@@ -1,15 +1,18 @@
 package com.example.cheermeup.fragment;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cheermeup.R;
+import com.example.cheermeup.activity.SelectPhotoActivity;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -23,7 +26,17 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_settings, parent, false);
+        final View inputView =  inflater.inflate(R.layout.fragment_settings, parent, false);
+
+        Button selectPhotoButton = inputView.findViewById(R.id.add_photos_button);
+        selectPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(inputView.getContext(), SelectPhotoActivity.class);
+                startActivity(intent);
+            }
+        });
+        return inputView;
     }
 
     @Override
@@ -43,4 +56,7 @@ public class SettingsFragment extends Fragment {
         SettingsRecyclerViewDataAdapter settingsDataAdapter = new SettingsRecyclerViewDataAdapter(photoItemList);
         settingsRecyclerView.setAdapter(settingsDataAdapter);
     }
+
+
+
 }
