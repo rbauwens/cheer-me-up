53bf4a0b6a6062476d43ea263988436ad023619d
rbauwens <ruthbauwens@hotmail.com>
2019-06-09 15:57:17 +0100

    Select a photo from device

--- app/src/main/java/com/example/cheermeup/activity/SelectPhotoActivity.java
+++ app/src/main/java/com/example/cheermeup/activity/SelectPhotoActivity.java
@@ -2,16 +2,49 @@ package com.example.cheermeup.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
 
 import com.example.cheermeup.R;
 
+import java.io.IOException;
+
 public class SelectPhotoActivity extends AppCompatActivity {
+
+    public final static int PICK_PHOTO_CODE = 1046;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_photo);
 
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            Uri photoUri = data.getData();
+            // Do something with the photo based on Uri
+            Bitmap selectedImage = null;
+            try {
+                selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // Load the selected image into a preview
+            ImageView ivPreview = findViewById(R.id.ivPreview);
+            ivPreview.setImageBitmap(selectedImage);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
